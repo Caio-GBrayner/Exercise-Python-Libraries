6935f22c255dd8b3fc83fb29922be39352f72f21
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-10 14:52:46 -0300

    database package -> db_operation and db_connection/ config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'dev',
+    'password' : 'dev123456',
+    'database': 'library_db'
+}
---
+++ database/db_connection.py
@@ -0,0 +1,10 @@
+
+from config import DB_CONFIG
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(**DB_CONFIG)
+        return connection
+    except mysql.connector.Error as err:
+        print(f"Error connecting to database: {err}")
+        return None
