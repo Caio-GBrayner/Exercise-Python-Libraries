1b02ce0ccebea0c5d5766b512d83102e905e6708
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-11 13:28:51 -0300

    member.py -> SQLALchemy config, hashcode and equals

--- entities/member.py
+++ entities/member.py
@@ -1,6 +1,18 @@
-class Member:
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    def __init__ (self, id: int, name: str, email: str, phone: str):
+Base = declarative_base
+
+class Member(Base):
+
+    __tablename__ = 'member'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phone = Column(String(20), nullable=False)
+
+    def __init__ (self, name: str, email: str, phone: str, id: int = None):
         self.__id = id
         self.__name = name
         self.__email = email
@@ -41,6 +53,14 @@ class Member:
             "email": self.__email,
             "phone": self.__phone
         }
+    
+    def __eq__(self, other):
+        if not isinstance(other, Member):
+            return False
+        return self.id == other.id and self.name == other.name and self.email == other.email and self.phone == other.phone
+
+    def __hash__(self):
+        return hash((self.id, self.name, self.email, self.phone))
 
     def __str__(self):
         return f"ID: {self.__id}, Name: {self.__name}, Email: {self.__email}, Phone: {self.__phone}"
