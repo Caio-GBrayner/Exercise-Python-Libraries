194084896a6ea27a4b731d4ff56f0589b6e4dab0
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-11 12:49:36 -0300

    repositry.py -> abstract class, member_repository -> inherits from the class repository

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,3 +1,12 @@
-from abc import ABC, abstractclassmethod
+
 from sqlalchemy.orm import Session
-from entities import Member
+from entities.member import Member
+from .repository import Repository
+
+
+class MemberRepository(Repository):
+    def __init__(self, session: Session):
+        super().__init__(session, Member)
+
+    def find_by_email(self, email: str):
+         return self.session.query(Member).filter(Member.email == email).first()
--- repositories/repository.py
+++ repositories/repository.py
@@ -1,10 +1,12 @@
+from abc import ABC, abstractclassmethod
 from sqlalchemy.orm import Session
 
-class Repository:
+class Repository(ABC):
     
     def __init__(self, session: Session, model):
         self.session = session
 
+    @abstractclassmethod
     def save(self, entity):
         self.session.add(entity)
         self.session.commit()
@@ -12,17 +14,21 @@ class Repository:
         
         return entity
     
+    @abstractclassmethod
     def find_by_id(self, id):
         return self.session.query(self.model).get(id)
     
+    @abstractclassmethod
     def find_all(self):
         return self.session.query(self.model).all()
     
+    @abstractclassmethod
     def update(self, entity):
         self.session.commit()
         self.session.refresh(entity)
         return entity
     
+    @abstractclassmethod
     def delete(self, entity):
         self.session.delete(entity)
         self.session.commit()
